09e4138c57fd8c10359213b467988b0c95f161d2
Daniel <dzmsgbox@gmail.com>
2019-09-29 01:52:11 +0200

    update after amendments, version 2.0

--- charcountf.py
+++ charcountf.py
@@ -4,13 +4,26 @@
 #<arg> value is optional. A Boolean. False will sort ascending, True will sort descending. Default is False
 #Dictionary methods were used.
 
-def chartxtcount(text, arg=False):
-    septext="".join(text)
-    dic= {}
-    resultscolumn=''
-    for i in sorted(septext,reverse=arg):
-        dic.setdefault(i, text.count(i))
-    for j,k in dic.items():
-        resultscolumn+="'"+j+"'"+' = '+str(k)+'\n'
-    return resultscolumn
+class CharacterCounter:
+    
+    def __init__(self, text):
+        self.text = text
+        self.lenght_of_text = 0
 
+    def chartxtcount(self, arg=False):
+        septext = "".join(self.text)
+        dic = {}
+        resultscolumn = []
+        if self.text:
+            for i in sorted(septext,reverse=arg):
+                dic.setdefault(i, self.text.count(i))
+                if i != "\n":
+                    self.lenght_of_text+=1
+            for j,k in dic.items():
+                if j == "\n":
+                    resultscolumn.append("Paragraphs" + ' = ' + str(k+1))
+                else:
+                    resultscolumn.append("'" + j + "'" + ' = ' + str(k))
+            return "\n".join(resultscolumn)
+        else:
+            return "Enter any text"
--- charctool.py
+++ charctool.py
@@ -1,62 +1,65 @@
 from tkinter import *
-import charcountf
+from charcountf import CharacterCounter
 
 class Application(Frame):
+
     def __init__(self, master):
         super(Application, self).__init__(master)
         self.grid()
-        self.label1()
-        self.labels()
-        self.user_text()
-        self.usrtxtdisp()
+        self.labels(0)
+        self.usertxt()
         self.asc_desc()
         self.buttons()
         self.results()
         self.confirm_clear()
-    def label1(self,lab='SUM OF CHARACTERS: 0'):      
-        Label(self, text=lab, font='Ubuntu 12 bold').grid(row=32, column=1,sticky=W)       
-    def labels(self,lab='xxx'):    
+
+
+    def labels(self, char_sum):  
+        Label(self, text="SUM OF CHARACTERS: {}           ".format(char_sum), font='Ubuntu 12 bold').grid(row=32, column=1,sticky=W) 
         Label(self, text="  ").grid(row=0, column=0)
         Label(self, text="  ").grid(row=0, column=3)
         Label(self, text="RESULTS: ",font='Ubuntu 12 bold').grid(row=1, column=4, sticky=W)
-        Label(self, text="Enter yout text here and confirm:").grid(row=0, column=1, sticky=E)
-    def user_text(self):
-        self.usrtxt=Entry(self)
-        self.usrtxt.grid(row=0, column=2, sticky=E)
-    def usrtxtdisp(self):
-        self.usrtxtdis=Text(self, width=50, height=30, wrap=WORD)
-        self.usrtxtdis.grid(row=2, column=1, rowspan=30, columnspan=2)
-        self.usrtxtdis.delete(0.0, END)
-        self.usrtxtdis.insert(0.0, '...the text will be displayed here...')
+        Label(self, text="Enter yout text here and confirm:").grid(row=1, column=1, sticky=W)
+
+
+    def usertxt(self):
+        self.usrtxt=Text(self, width=50, height=30, wrap=WORD)
+        self.usrtxt.grid(row=2, column=1, rowspan=30, columnspan=2)
+
+
     def buttons(self):
         Button(self, text='Confirm', command=self.confirm).grid(row=1,column=2, sticky=E)
         Button(self, text='Clear', command=self.confirm_clear).grid(row=1,column=1, sticky=E)
+
+        
     def results(self):
         self.outputtxt=Text(self, width=20, height=30, wrap=WORD)
         self.outputtxt.grid(row=2, rowspan=30, column=4)
+
+
     def asc_desc(self):
         self.var=BooleanVar()
         self.var.set(True)
-        self.rad1=Radiobutton(self, text='ascending', variable=self.var, value=False, command=self.confirm).grid(row=3, column=5, sticky =W)
-        self.rad2=Radiobutton(self, text='descending', variable=self.var, value=True, command=self.confirm).grid(row=4, column=5, sticky =W)
+        Radiobutton(self, text='ascending', variable=self.var, value=False, command=self.confirm).grid(row=3, column=5, sticky =W)
+        Radiobutton(self, text='descending', variable=self.var, value=True, command=self.confirm).grid(row=4, column=5, sticky =W)
+
+
     def confirm(self):
-        txt=self.usrtxt.get()
-        self.usrtxtdis.delete(0.0, END)
-        if txt:
-            self.usrtxtdis.insert(0.0, txt)
-        else:
-            self.usrtxtdis.insert(0.0, '...the text will be displayed here...')
+        txt = CharacterCounter((self.usrtxt.get("0.0", END))[:-1]) #[:-1] removes from the text 'enter character' introduced by GUI
         self.outputtxt.delete(0.0, END)
-        self.outputtxt.insert(0.0, charcountf.chartxtcount(txt, self.var.get()))
-        y="SUM OF CHARACTERS: %s"%(str(len(txt))+'        ')
-        self.label1(y)
+        self.outputtxt.insert(0.0, txt.chartxtcount(self.var.get()))
+        self.char_sum=str(txt.lenght_of_text)
+        self.labels(self.char_sum)
+
+
     def confirm_clear(self):
-        self.usrtxt.delete(0, END)
-        self.usrtxtdis.delete(0.0, END)
-        self.usrtxtdis.insert(0.0, '...the text will be displayed here...')
+        self.usrtxt.delete(0.0, END)
+        #self.sum = Label(self, text="SUM OF CHARACTERS: 0           ", font='Ubuntu 12 bold').grid(row=32, column=1,sticky=W)
+        self.char_sum=0
+        self.labels(self.char_sum)
 
 root = Tk()
-root.title('Character Count Tool 1.0')
+root.title('Character Count Tool 2.0')
 root.geometry("800x600")
 app = Application(root)
 
